33782c495b0e6f0ddc206bc7c5365f5b0a07f380
venugopalgodavarthi <venugopalgodavarthi@gmail.com>
2023-11-07 12:02:26 +0530

    update

--- authe/urls.py
+++ authe/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from authe.views import register_view, login_view, home_view, logout_view
+from authe.views import register_view, login_view, home_view, logout_view, otp_view
 app_name = 'authe'
 
 urlpatterns = [
     path(route='register/', view=register_view, name="register"),
     path(route='login/', view=login_view, name="login"),
+    path(route='otp/', view=otp_view, name="otp"),
     path(route='home/', view=home_view, name="home"),
     path(route='logout/', view=logout_view, name="logout"),
 ]
--- authe/views.py
+++ authe/views.py
@@ -4,6 +4,12 @@ from authe.models import register_model
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.conf import settings
+import random
+from django.contrib import messages
+
 # Create your views here.
 
 
@@ -15,11 +21,19 @@ def register_view(request):
         form = register_form(request.POST, request.FILES)
         if form.is_valid:
             form.save()
-            return redirect('/login')
+            send_mail(subject="welcome to myproject",
+                      message=f"Hello {form.cleaned_data['first_name']}", from_email=settings.EMAIL_HOST_USER, recipient_list=[form.cleaned_data['email'],])
+            messages.success("Registration process is success")
+            return redirect('/authe/login')
     return render(request=request, template_name="register.html", context={'form': form})
 
 
+user_id = None
+otp_confirm = None
+
+
 def login_view(request):
+    global user_id, otp_confirm
     form = AuthenticationForm()
     if request.method == 'POST':
         print(request.POST)
@@ -27,18 +41,40 @@ def login_view(request):
         if form.is_valid:
             user = authenticate(
                 request, username=request.POST['username'], password=request.POST['password'])
-            print(user, user.email)
-            login(request, user)
-            return redirect('/authe/home')
+            if user:
+                data = random.randrange(100000, 900000)
+                send_mail(subject="welcome to myproject",
+                          message=f"""haii otp sent to {user.email}
+                        current otp: {data}
+                        thank you""", from_email=settings.EMAIL_HOST_USER, recipient_list=[user.email])
+                user_id = user
+                otp_confirm = data
+                messages.success(request, "otp sent")
+                return redirect('/authe/otp')
+            else:
+                messages.error(request, "Invalid login")
     return render(request=request, template_name="login.html", context={'form': form})
 
 
+def otp_view(request):
+    if request.method == 'POST':
+        if str(request.POST['otp']) == str(otp_confirm):
+            login(request, user_id)
+            messages.success(request, "Login success")
+            return redirect('/authe/home')
+        else:
+            return redirect('/authe/otp')
+    return render(request=request, template_name='otp.html')
+
+
+@login_required(login_url='/authe/login')
 def home_view(request):
     print(request.user.id)
     res = register_model.objects.get(id=request.user.id)
     return render(request=request, template_name="home.html", context={'res': res})
 
 
+@login_required(login_url='/authe/login')
 def logout_view(request):
     logout(request)
     return redirect('/authe/login')
